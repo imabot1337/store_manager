652ae722ce6b7389f7328f16f7f6c6d33d9e849e
Alin Boca <43600694+imabot1337@users.noreply.github.com>
2023-12-05 17:46:36 +0200

    add spring web, jpa, h2 database

---
+++ store_manager/src/main/java/store_manager/com/example/store_manager/StoreManagerApplication.java
@@ -0,0 +1,13 @@
+package store_manager.com.example.store_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagerApplication.class, args);
+	}
+
+}
---
+++ store_manager/src/test/java/store_manager/com/example/store_manager/StoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package store_manager.com.example.store_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
